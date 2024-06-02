c4d613ea7020091a2364bef890f49db0db31b3c3
Steffen Wesiz <steffen.weisz@capgemini.com>
2024-06-02 10:04:23 +0200

    Initial commit

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      welcome works!
+    </p>
+  `,
+  styles: ``
+})
+export class WelcomeComponent {
+
+}
